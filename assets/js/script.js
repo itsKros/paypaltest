a6eb7ed09a7b403e5b721b67cf5769854ed501d8
itsKros <kroshan197@gmail.com>
2018-08-11 14:23:52 +0530

    Table Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,36 @@ $( document ).ready(function() {
 
 
       $("#tbl").on("click", "td", function(){
-        var firstCol = $(this).parent("tr").find("th:first").text();
-        var firstRow = $(this).parent().parent().children().index($(this).parent().text());
-        //console.log(firstCol);
-        console.log(firstRow);
+        var urgency = $(this).parent("tr").find("th:first").text();
+        var currentPrice = $(this).text();
+        var currentPriceIndex = $(this).index()+1;
+        var level = $("#tbl>thead>tr>th:nth-child(" + currentPriceIndex + ")").text();
+
+        
+        $('.order-urgency span').html(urgency);
+        $('.order-level span').html(level);
+        $('.order-total span').html(currentPrice);
+        
+      });
+
+
+      $("#btn").click(function() {
+        var oU=$('.order-urgency span').text();
+        var ol=$('.order-level span').text();
+        var ot=$('.order-total span').text();
+       
+        $.ajax ({
+            url: "index.php",
+            data: { 
+              oU : oU,
+              ol : ol,
+              ot : ot,
+             },
+            success: function( result ) {
+                alert("Hi, testing");
+                alert( result );
+            }
+        });
+        window.location(index.php);
       });
 });
--- price.php
+++ price.php
@@ -2,151 +2,49 @@
 
     <div class="container">
         <div class="row py-5">
-            <div class="col-lg-8 offset-md-2 text-left">
-            <table class="table" id="tbl">
-                <thead class=".thead-light">
-                    <tr> 
-                        <th scope="col" class="urgent">Urgency</th>
-                        <th scope="col">High School</th>
-                        <th scope="col">College</th>
-                        <th scope="col">Undergraduate</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="col" class="urgent">20 Days</th>
-                        <td>$35.61</td>
-                        <td>$46.95</td>
-                        <td>$53.88</td>
-                    </tr>
-                    <tr>
-                        <th scope="col" class="urgent">14 Days</th>
-                        <td>$39.54</td>
-                        <td>$50.10</td>
-                        <td>$56.70</td>
-                    </tr>
-                    <tr>
-                    <th scope="col" class="urgent">10 Days</th>
-                        <td>$41.85</td>
-                        <td>$50.85</td>
-                        <td>$56.85</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="col-lg-8 text-left">
+                <table class="table" id="tbl">
+                    <thead class=".thead-light">
+                        <tr> 
+                            <th scope="col" class="urgent">Urgency</th>
+                            <th scope="col">High School</th>
+                            <th scope="col">College</th>
+                            <th scope="col">Undergraduate</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="col" class="urgent">20 Days</th>
+                            <td>$35.61</td>
+                            <td>$46.95</td>
+                            <td>$53.88</td>
+                        </tr>
+                        <tr>
+                            <th scope="col" class="urgent">14 Days</th>
+                            <td>$39.54</td>
+                            <td>$50.10</td>
+                            <td>$56.70</td>
+                        </tr>
+                        <tr>
+                        <th scope="col" class="urgent">10 Days</th>
+                            <td>$41.85</td>
+                            <td>$50.85</td>
+                            <td>$56.85</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
 
-
-
-
-<table class="table" cellpadding="0" cellspacing="0">
-    <thead>
-        <tr id="table-head">
-            <td>Urgency</td>
-            <td level-work="1" class="">High School</td>
-            <td level-work="2" class="">College</td>
-            <td level-work="9" class="">Undergraduate</td>
-            <td level-work="7" class="">Master</td>
-            <td level-work="8">Ph.D.</td>
-        </tr>
-    </thead>
-    <tbody id="table-body">
-        <tr>
-            <td class="urgency-cell" urgency="12">20 days</td>
-            <td level-work="1" urgency="12" class=""><a href="/order?doctype=20&amp;urgency=12&amp;level=1&amp;numpages=1">$11.86</a></td>
-            <td level-work="2" urgency="12" class=""><a href="/order?doctype=20&amp;urgency=12&amp;level=2&amp;numpages=1">$15.64</a></td>
-            <td level-work="9" urgency="12" class=""><a href="/order?doctype=20&amp;urgency=12&amp;level=9&amp;numpages=1">$17.96</a></td>
-            <td level-work="7" urgency="12" class=""><a href="/order?doctype=20&amp;urgency=12&amp;level=7&amp;numpages=1">$24.04</a></td>
-            <td level-work="8" urgency="12"><a href="/order?doctype=20&amp;urgency=12&amp;level=8&amp;numpages=1">$25.10</a></td>
-        </tr>
-        <tr>
-            <td class="urgency-cell" urgency="11">14 days</td>
-            <td level-work="1" urgency="11" class=""><a href="/order?doctype=20&amp;urgency=11&amp;level=1&amp;numpages=1">$13.18</a></td>
-            <td level-work="2" urgency="11" class=""><a href="/order?doctype=20&amp;urgency=11&amp;level=2&amp;numpages=1">$16.70</a></td>
-            <td level-work="9" urgency="11" class=""><a href="/order?doctype=20&amp;urgency=11&amp;level=9&amp;numpages=1">$18.90</a></td>
-            <td level-work="7" urgency="11" class=""><a href="/order?doctype=20&amp;urgency=11&amp;level=7&amp;numpages=1">$25.15</a></td>
-            <td level-work="8" urgency="11"><a href="/order?doctype=20&amp;urgency=11&amp;level=8&amp;numpages=1">$26.14</a></td>
-        </tr>
-        <tr>
-            <td class="urgency-cell" urgency="10">10 days</td>
-            <td level-work="1" urgency="10" class=""><a href="/order?doctype=20&amp;urgency=10&amp;level=1&amp;numpages=1">$14.65</a></td>
-            <td level-work="2" urgency="10" class=""><a href="/order?doctype=20&amp;urgency=10&amp;level=2&amp;numpages=1">$17.80</a></td>
-            <td level-work="9" urgency="10" class=""><a href="/order?doctype=20&amp;urgency=10&amp;level=9&amp;numpages=1">$19.90</a></td>
-            <td level-work="7" urgency="10" class=""><a href="/order?doctype=20&amp;urgency=10&amp;level=7&amp;numpages=1">$26.20</a></td>
-            <td level-work="8" urgency="10"><a href="/order?doctype=20&amp;urgency=10&amp;level=8&amp;numpages=1">$27.25</a></td>
-        </tr>
-        <tr>
-            <td class="urgency-cell selected-urgency" urgency="9">7 days</td>
-            <td level-work="1" urgency="9" class=""><a href="/order?doctype=20&amp;urgency=9&amp;level=1&amp;numpages=1">$15.95</a></td>
-            <td level-work="2" urgency="9" class="selected-price"><a href="/order?doctype=20&amp;urgency=9&amp;level=2&amp;numpages=1">$17.95</a></td>
-            <td level-work="9" urgency="9" class=""><a href="/order?doctype=20&amp;urgency=9&amp;level=9&amp;numpages=1">$19.95</a></td>
-            <td level-work="7" urgency="9" class=""><a href="/order?doctype=20&amp;urgency=9&amp;level=7&amp;numpages=1">$25.95</a></td>
-            <td level-work="8" urgency="9"><a href="/order?doctype=20&amp;urgency=9&amp;level=8&amp;numpages=1">$26.95</a></td>
-        </tr>
-        <tr>
-            <td class="urgency-cell" urgency="8">5 days</td>
-            <td level-work="1" urgency="8" class=""><a href="/order?doctype=20&amp;urgency=8&amp;level=1&amp;numpages=1">$17.95</a></td>
-            <td level-work="2" urgency="8" class=""><a href="/order?doctype=20&amp;urgency=8&amp;level=2&amp;numpages=1">$18.95</a></td>
-            <td level-work="9" urgency="8" class=""><a href="/order?doctype=20&amp;urgency=8&amp;level=9&amp;numpages=1">$20.95</a></td>
-            <td level-work="7" urgency="8" class=""><a href="/order?doctype=20&amp;urgency=8&amp;level=7&amp;numpages=1">$26.95</a></td>
-            <td level-work="8" urgency="8"><a href="/order?doctype=20&amp;urgency=8&amp;level=8&amp;numpages=1">$27.95</a></td>
-        </tr>
-        <tr>
-            <td class="urgency-cell" urgency="7">4 days</td>
-            <td level-work="1" urgency="7" class=""><a href="/order?doctype=20&amp;urgency=7&amp;level=1&amp;numpages=1">$18.95</a></td>
-            <td level-work="2" urgency="7" class=""><a href="/order?doctype=20&amp;urgency=7&amp;level=2&amp;numpages=1">$19.95</a></td>
-            <td level-work="9" urgency="7" class=""><a href="/order?doctype=20&amp;urgency=7&amp;level=9&amp;numpages=1">$21.95</a></td>
-            <td level-work="7" urgency="7" class=""><a href="/order?doctype=20&amp;urgency=7&amp;level=7&amp;numpages=1">$27.95</a></td>
-            <td level-work="8" urgency="7"><a href="/order?doctype=20&amp;urgency=7&amp;level=8&amp;numpages=1">$28.95</a></td>
-        </tr>
-        <tr>
-            <td class="urgency-cell" urgency="6">3 days</td>
-            <td level-work="1" urgency="6" class=""><a href="/order?doctype=20&amp;urgency=6&amp;level=1&amp;numpages=1">$20.95</a></td>
-            <td level-work="2" urgency="6" class=""><a href="/order?doctype=20&amp;urgency=6&amp;level=2&amp;numpages=1">$21.95</a></td>
-            <td level-work="9" urgency="6" class=""><a href="/order?doctype=20&amp;urgency=6&amp;level=9&amp;numpages=1">$22.95</a></td>
-            <td level-work="7" urgency="6" class=""><a href="/order?doctype=20&amp;urgency=6&amp;level=7&amp;numpages=1">$28.95</a></td>
-            <td level-work="8" urgency="6"><a href="/order?doctype=20&amp;urgency=6&amp;level=8&amp;numpages=1">$31.95</a></td>
-        </tr>
-        <tr>
-            <td class="urgency-cell" urgency="5">48 hours</td>
-            <td level-work="1" urgency="5" class=""><a href="/order?doctype=20&amp;urgency=5&amp;level=1&amp;numpages=1">$22.95</a></td>
-            <td level-work="2" urgency="5" class=""><a href="/order?doctype=20&amp;urgency=5&amp;level=2&amp;numpages=1">$23.95</a></td>
-            <td level-work="9" urgency="5" class=""><a href="/order?doctype=20&amp;urgency=5&amp;level=9&amp;numpages=1">$24.95</a></td>
-            <td level-work="7" urgency="5" class=""><a href="/order?doctype=20&amp;urgency=5&amp;level=7&amp;numpages=1">$29.95</a></td>
-            <td level-work="8" urgency="5"><a href="/order?doctype=20&amp;urgency=5&amp;level=8&amp;numpages=1">$34.95</a></td>
-        </tr>
-        <tr>
-            <td class="urgency-cell" urgency="4">24 hours</td>
-            <td level-work="1" urgency="4" class=""><a href="/order?doctype=20&amp;urgency=4&amp;level=1&amp;numpages=1">$28.95</a></td>
-            <td level-work="2" urgency="4" class=""><a href="/order?doctype=20&amp;urgency=4&amp;level=2&amp;numpages=1">$29.95</a></td>
-            <td level-work="9" urgency="4" class=""><a href="/order?doctype=20&amp;urgency=4&amp;level=9&amp;numpages=1">$32.95</a></td>
-            <td level-work="7" urgency="4" class=""><a href="/order?doctype=20&amp;urgency=4&amp;level=7&amp;numpages=1">$33.95</a></td>
-            <td level-work="8" urgency="4"><a href="/order?doctype=20&amp;urgency=4&amp;level=8&amp;numpages=1">$38.95</a></td>
-        </tr>
-        <tr>
-            <td class="urgency-cell" urgency="3">12 hours</td>
-            <td level-work="1" urgency="3" class=""><a href="/order?doctype=20&amp;urgency=3&amp;level=1&amp;numpages=1">$30.95</a></td>
-            <td level-work="2" urgency="3" class=""><a href="/order?doctype=20&amp;urgency=3&amp;level=2&amp;numpages=1">$32.95</a></td>
-            <td level-work="9" urgency="3" class=""><a href="/order?doctype=20&amp;urgency=3&amp;level=9&amp;numpages=1">$34.95</a></td>
-            <td level-work="7" urgency="3" class=""><a href="/order?doctype=20&amp;urgency=3&amp;level=7&amp;numpages=1">$35.95</a></td>
-            <td level-work="8" urgency="3"><a href="/order?doctype=20&amp;urgency=3&amp;level=8&amp;numpages=1">$40.95</a></td>
-        </tr>
-        <tr class="price-table-content-last-row">
-            <td class="urgency-cell" urgency="2">6 hours</td>
-            <td level-work="1" urgency="2"><a href="/order?doctype=20&amp;urgency=2&amp;level=1&amp;numpages=1">$33.95</a></td>
-            <td level-work="2" urgency="2"><a href="/order?doctype=20&amp;urgency=2&amp;level=2&amp;numpages=1">$35.95</a></td>
-            <td level-work="9" urgency="2"><a href="/order?doctype=20&amp;urgency=2&amp;level=9&amp;numpages=1">$36.95</a></td>
-            <td level-work="7" urgency="2"><a href="/order?doctype=20&amp;urgency=2&amp;level=7&amp;numpages=1">$37.95</a></td>
-            <td level-work="8" urgency="2"><a href="/order?doctype=20&amp;urgency=2&amp;level=8&amp;numpages=1">$42.95</a></td>
-        </tr>
-        <tr>
-            <td class="urgency-cell" urgency="1">3 hours</td>
-            <td level-work="1" urgency="1"><a href="/order?doctype=20&amp;urgency=1&amp;level=1&amp;numpages=1">$34.95</a></td>
-            <td level-work="2" urgency="1"><a href="/order?doctype=20&amp;urgency=1&amp;level=2&amp;numpages=1">$36.95</a></td>
-            <td level-work="9" urgency="1"><a href="/order?doctype=20&amp;urgency=1&amp;level=9&amp;numpages=1">$38.95</a></td>
-            <td level-work="7" urgency="1"><a href="/order?doctype=20&amp;urgency=1&amp;level=7&amp;numpages=1">$40.95</a></td>
-            <td level-work="8" urgency="1"><a href="/order?doctype=20&amp;urgency=1&amp;level=8&amp;numpages=1">$44.95</a></td>
-        </tr>
-    </tbody>
-</table>
+            <div class="col-lg-4 text-left">
+                <div class="card">
+                    <div class="card-header"><h5>Order Total</h5></div>
+                    <div class="card-body">
+                        <h6 class="order-urgency">Urgency: <span></span></h6>
+                        <h6 class="order-level">Level: <span></span></h6>
+                        <h6 class="order-total">Total: <span></span></h6>
+                    </div>
+                    <div class="card-footer"><button class="btn btn-primary">Order</button></div>
+                </div>
             </div>
         </div>
     </div>
